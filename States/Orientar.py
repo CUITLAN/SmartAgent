63fb0c03765227854239e1fc49d6205a34984405
Cuitlan <alan78707@gmail.com>
2025-03-20 15:33:51 +0100

    Added: Agente con Todos los estados Implementados

--- States/Orientar.py
+++ States/Orientar.py
@@ -5,15 +5,31 @@ class Orientar(State):
 
     def __init__(self, id):
         super().__init__(id)
+        self.orientado = False 
+
     def Start(self):
         print("Estado: Orientar iniciado")
+        self.orientado = False 
+
     def Update(self, perception):
         percepciones = Diccionario(perception)  
-        print("Orientando 🧭-🧭-🧭-🧭-🧭-🧭-🧭-🧭-🧭-🧭")
-        action, Shot = percepciones.GirarAbalaDerecha()
-        return action, Shot
+        print("Orientando")
+
+        if not self.orientado:  
+            action, Shot = percepciones.GirarHaciaBala()
+            self.orientado = True 
+            return action, Shot  
+        else:
+            return 0, False 
+
     def Transit(self, perception):   
+        percepciones = Diccionario(perception)  
+        if percepciones.veJugadorEnEjeY or percepciones.veJugadorEnEjeY:
+            return "Disparar"
+        else:
+            return "Explorar"
     
-        return "Disparar"
+         
+
     def End(self):
-        return print("Saliendo del estado de explorar")
+        print("Saliendo del estado de Orientar")
